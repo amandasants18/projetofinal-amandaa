953904f57e894850951f397f9a05f549dec9b462
Amanda Santos <amandasantosvip@hotmail.com>
2021-09-04 16:06:35 -0300

    Alterando clientesRoutes

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -5,34 +5,34 @@ const Clientes = require('../models/clientes')
 const controller = require("../controllers/clientesController")
 
 
-//listar todos os estudios/get/find
-router.get('/', async (req, res) => {
-  const clientes = await Clientes.find()
-  res.json(clientes)
-})
+// //listar todos os estudios/get/find
+// router.get('/', async (req, res) => {
+//   const clientes = await Clientes.find()
+//   res.json(clientes)
+// })
 
-//criar um novo estudio/post/save
-router.post('/', async (req, res) => {
-  const clientes = new Clientes({
-    _id: new mongoose.Types.ObjectId(),
-    nome: req.body.nome,
-    criadoEm: req.body.criadoEm,
-  })
+// //criar um novo estudio/post/save
+// router.post('/', async (req, res) => {
+//   const clientes = new Clientes({
+//     _id: new mongoose.Types.ObjectId(),
+//     nome: req.body.nome,
+//     criadoEm: req.body.criadoEm,
+//   })
  
-   const clienteJaExiste = await Clientes.findOne({nome: req.body.nome})
-   if (clienteJaExiste) {
-     return res.status(409).json({error: 'Cliente ja cadastrado.'})
-  }
-//  try{
+//    const clienteJaExiste = await Clientes.findOne({nome: req.body.nome})
+//    if (clienteJaExiste) {
+//      return res.status(409).json({error: 'Cliente ja cadastrado.'})
+//   }
+// //  try{
 
-//      const novoCliente = await cliente.save()
-//      res.status(201).json(novoCliente)
+// //      const novoCliente = await cliente.save()
+// //      res.status(201).json(novoCliente)
     
-//     }catch(err){
+// //     }catch(err){
 
-//     res.status.apply(400).json({message: err.message})
-//  }
-})
+// //     res.status.apply(400).json({message: err.message})
+// //  }
+// })
 
 router.get("/oi", (req, resp)=>{
   resp.status(200).send({"mensagem":"oi to aqui ta funcionando "})
@@ -43,7 +43,8 @@ router.get("/oi", (req, resp)=>{
 //listar um estudio/get/findById
 
 //atualizar uma informacao especifica num estudio/patch/findById/save
-router.post("/create", controller.createClientes)
+router.post("/", controller.createClientes)
+router.get("/", controller.getAll)
 // router.patch('/:id', controller.updateCliente)
 
 
