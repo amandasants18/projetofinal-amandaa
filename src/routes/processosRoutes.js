73ea6aa7eafed8f212abd64474ef313c452ac7ba
Amanda Santos <amandasantosvip@hotmail.com>
2021-09-07 16:15:15 -0300

    Alterando processos Routes

--- src/routes/processosRoutes.js
+++ src/routes/processosRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const controller = require("../controllers/processosController")
 
-
+router.get("/", controller.getAllProcessos)
 router.post("/create", controller.createProcesso)
 
 
