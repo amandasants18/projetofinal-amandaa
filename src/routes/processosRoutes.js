a24cca0a30f570a40cd9cdcbf7cf63952c71f827
Amanda Santos <amandasantosvip@hotmail.com>
2021-09-09 19:55:36 -0300

    Subindo nova alteracao

--- src/routes/processosRoutes.js
+++ src/routes/processosRoutes.js
@@ -7,7 +7,7 @@ router.get("/", controller.getAllProcessos)
 router.post("/create", controller.createProcesso)
 router.delete("/:id", controller.deleteProcesso)
 router.get("/:cliente", controller.getProcessosPorCliente)
-router.patch("/id", controller.updateProcesso )
+router.patch("/:id", controller.updateProcesso )
  
 
 
